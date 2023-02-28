d145bc3a9ffebe93f86c0dcfabe75d1016e492d9
neiderruiz32 <neiderruizgarcias@gmail.com>
2023-02-27 22:07:40 -0500

    fix: se agrego URL para manejo de cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule, {
       cors: {
-        origin: 'http://127.0.0.1:5173',
+        origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
         credentials: true,
       },
     });
